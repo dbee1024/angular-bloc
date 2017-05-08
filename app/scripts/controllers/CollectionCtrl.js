b1cdc7a08392634b2e2c3069aa2fe36e38a248e2
dbee1024 <David@Davids-MacBook-Pro.local>
2017-05-07 20:47:34 -0700

    controller added

---
+++ app/scripts/controllers/CollectionCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+     function CollectionCtrl() {
+         this.albums = [];
+         for (var i=0; i < 12; i++) {
+            this.albums.push(angular.copy(albumPicasso));
+        }
+     }
+ //enisp
+     angular.module('blocJams')
+         .controller('CollectionCtrl', CollectionCtrl);
+ })();
